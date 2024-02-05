1d16e52fa21c5cf717e74a910bc89a8512732331
abdullahalnomanfb <abdullahalnoman1512@gmail.com>
2024-02-05 21:30:27 +0600

    [Axios interceptors has been added]

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,13 +5,15 @@ import { z } from 'zod';
 dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 const envVarsZodSchema = z.object({
-    NODE_ENV: z.string(),
-    PORT: z
-        .string()
-        .default('3030')
-        .refine((val) => Number(val)),
-    JWT_SECRET: z.string(),
-    REDIS_URL:z.string()
+  NODE_ENV: z.string(),
+  PORT: z
+    .string()
+    .default('3030')
+    .refine((val) => Number(val)),
+  JWT_SECRET: z.string(),
+  REDIS_URL: z.string(),
+  AUTH_SERVICE_URL: z.string(),
+  CORE_SERVICE_URL:z.string(),
 });
 
 const envVars = envVarsZodSchema.parse(process.env);
@@ -24,5 +26,7 @@ export default {
     },
     redis:{
         url:envVars.REDIS_URL
-    }
+    },
+    authServiceUrl:envVars.AUTH_SERVICE_URL,
+    coreServiceUrl:envVars.CORE_SERVICE_URL
 };
--- src/shared/redis.ts
+++ src/shared/redis.ts
@@ -6,12 +6,14 @@ let redisClient = createClient({
   url: config.redis.url
 });
 
-redisClient.on('error',(err)=>logger.error('Redis error: ' ,err))
-redisClient.on('connect',(err)=>logger.info("Redis connected"))
 
 const connect = async(): Promise<void> =>{
     await redisClient.connect();
 }
+
+redisClient.on('error', (err) => logger.error('Redis error: ', err));
+redisClient.on('connect', (err) => logger.info('Redis connected'));
+
 
 export const RedisClient ={
     connect
